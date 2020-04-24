20d0a439c5b95d369f55fcb1a0d8fd86ede0c997
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-24 11:19:54 +0100

    TF files edited

--- ReadMe.md
+++ ReadMe.md
@@ -4,4 +4,4 @@ Terraform is an Orchestration Tool, that is part of infrastructure as code.
 
 Where Chef and Packer sit more on **Configuration Management** Side and create immutable AMIs.
 
-Terraform sits on the orchestration side. This includes the creation of networks and complex systems and deploys AMIs
+Terraform sits on the **orchestration side**. This includes the creation of networks and complex systems and deploys AMIs
--- main.tf
+++ main.tf
@@ -4,22 +4,23 @@ provider "aws" {
 }
 
 # Create VPS
-resource "aws_vpc" "App_VPC" {
-  cidr_block = "10.0.0.0/16"
-
-
-  tags = {
-    Name = "Eng54_VS_App_VPC"
-  }
-}
+# resource "aws_vpc" "App_VPC" {
+#   cidr_block = "10.0.0.0/16"
+#
+#
+#   tags = {
+#     Name = "Eng54_VS_App_VPC"
+#   }
+# }
 
 # Launching and Instance
+# ami-054778af2ffd719dd
 
 resource "aws_instance" "app_instance" {
-    ami = "ami-054778af2ffd719dd"
+    ami = "ami-040bb941f8d94b312"
     instance_type = "t2.micro"
     associate_public_ip_address = true
-    tags ={
+    tags = {
       Name = "Eng54-Victor-App"
     }
   }
